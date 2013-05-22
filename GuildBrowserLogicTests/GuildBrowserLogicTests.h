86ef8069bf7c3dd34f5914165ce1a4d63ffa7443
Joey <joey@955dreams.com>
2013-05-22 09:39:48 -0700

    logic tests

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  GuildBrowserLogicTests.h
+//  GuildBrowserLogicTests
+//
+//  Created by Joey Chang on 5/22/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  GuildBrowserLogicTests.m
+//  GuildBrowserLogicTests
+//
+//  Created by Joey Chang on 5/22/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import "GuildBrowserLogicTests.h"
+
+@implementation GuildBrowserLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GuildBrowserLogicTests");
+}
+
+@end
